94d50bfd4299fc7b1a75d0263b158166c4998fec
Olivier3456 <olivier.larrey@gmail.com>
2023-02-25 18:15:40 +0100

    Enemy can see and follow player

--- Assets/Scripts/NPCDisplacements.cs
+++ Assets/Scripts/NPCDisplacements.cs
@@ -6,7 +6,12 @@ using UnityEngine.AI;
 public class NPCDisplacements : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
-    private Vector3 agentDestination;
+    [SerializeField] private NPCVision npcVision;
+
+    [SerializeField] private float walkSpeed = 3.5f;
+    [SerializeField] private float runSpeed = 7f;
+    
+    
 
     void Start()
     {
@@ -16,25 +21,35 @@ public class NPCDisplacements : MonoBehaviour
 
     void Update()
     {
+        if (npcVision.CanSeeTarget())
+        {
+            Debug.Log("Déplacement vers la position actuelle de la cible.");
+            agent.SetDestination(npcVision.target.position);
+            agent.speed = runSpeed;
+        }
+
+
         if (agent.remainingDistance < 2)
         {
+            Debug.Log("Destination atteinte.");
             SetRandomDestinationOnTheNavMesh();
         }
     }
 
 
-    public void SetRandomDestinationOnTheNavMesh()
+    private void SetRandomDestinationOnTheNavMesh()
     {
         Vector3 newRandomDest = new Vector3(Random.Range(-50f, 50f), 0.5f, Random.Range(-50f, 50f));
 
 
-        // Vérifie si la destination est sur le NavMesh
+        // Vérifie si on peut trouver un point sur le NavMesh à moins de 2m de newRandomDest
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(newRandomDest, out hit, 0.1f, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(newRandomDest, out hit, 2.0f, NavMesh.AllAreas))
         {
-            // Définit la destination sur le NavMeshAgent
+            // Si oui, définit la destination sur le NavMeshAgent
             agent.SetDestination(hit.position);
-            Debug.Log("Nouvelle destination de l'agent : " + hit.position);
+            agent.speed = walkSpeed;
+            Debug.Log("Nouvelle destination random de l'agent : " + hit.position);
         }
         else SetRandomDestinationOnTheNavMesh();
     }
