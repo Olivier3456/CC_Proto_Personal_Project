b2019fb12864c92720f5d33fc51a905c38c9c148
Olivier3456 <olivier.larrey@gmail.com>
2023-02-26 19:39:28 +0100

    TRex model and sounds

--- Assets/Scripts/NPCDisplacements.cs
+++ Assets/Scripts/NPCDisplacements.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(AudioSource))]
 public class NPCDisplacements : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
@@ -10,29 +11,67 @@ public class NPCDisplacements : MonoBehaviour
 
     [SerializeField] private float walkSpeed = 3.5f;
     [SerializeField] private float runSpeed = 7f;
-    
-    
+
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip roarSound;
+    [SerializeField] private AudioClip stepSound;
+
+    private bool hasRoared;   
+    private float roarTimer;
+    private float roarDelay = 10;
+
+    private Vector3 lastStepPosition;
+    private float distanceBetweenSteps = 4;
+
+
 
     void Start()
     {
         SetRandomDestinationOnTheNavMesh();
+        audioSource = GetComponent<AudioSource>();
+
+        lastStepPosition = transform.position;
     }
 
 
     void Update()
     {
+        if (Vector3.Distance(lastStepPosition, transform.position) > distanceBetweenSteps)
+        {
+            audioSource.PlayOneShot(stepSound);
+            lastStepPosition = transform.position;
+        }
+
+
+
+        if (hasRoared)
+        {
+            roarTimer += Time.deltaTime;
+            if (roarTimer > roarDelay) hasRoared = false;
+        }
+
+
         if (npcVision.CanSeeTarget())
         {
             Debug.Log("Déplacement vers la position actuelle de la cible.");
             agent.SetDestination(npcVision.target.position);
             agent.speed = runSpeed;
+
+            if (!hasRoared)
+            {
+                audioSource.PlayOneShot(roarSound);
+                hasRoared = true;
+                roarTimer = 0;
+            }
         }
+        else hasRoared = false;
+
 
 
         if (agent.remainingDistance < 2)
         {
             Debug.Log("Destination atteinte.");
-            SetRandomDestinationOnTheNavMesh();
+            SetRandomDestinationOnTheNavMesh();            
         }
     }
 
